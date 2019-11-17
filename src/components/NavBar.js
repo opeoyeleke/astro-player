f007c16bbacd40613099c8f78f8db376317f99f0
9thtower <topfemo@gmail.com>
2019-11-17 20:30:15 +0100

    fixed mobile nav button size and more changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -73,21 +73,35 @@ export default class NavBar extends Component {
 
         <div className="mobile-menu">
           <Menu>
-            <a className="menu-item" href="/">
-              Home
-            </a>
-
-            <a className="menu-item" href="/albums">
-              Albums
-            </a>
-
-            <a className="menu-item" href="/artists">
-              Artists
-            </a>
-
-            <a className="menu-item" href="/songs">
-              Songs
-            </a>
+            <div className="top-nav">
+              <div className="nav-item">
+                <span className="nav-title">LIBRARY</span>
+              </div>
+              <div className="nav-item">
+                <NavLink to="/playlists" activeClassName="active">
+                  <Icon className="nav-icon" icon={ic_queue_music} />
+                  Playlists
+                </NavLink>
+              </div>
+              <div className="nav-item">
+                <NavLink to="/artists" activeClassName="active">
+                  <Icon className="nav-icon" icon={ic_person} />
+                  Artists
+                </NavLink>
+              </div>
+              <div className="nav-item">
+                <NavLink to="/albums" activeClassName="active">
+                  <Icon className="nav-icon" icon={ic_album} />
+                  Albums
+                </NavLink>
+              </div>
+              <div className="nav-item">
+                <NavLink to="/songs" activeClassName="active">
+                  <Icon className="nav-icon" icon={ic_music_note} />
+                  Songs
+                </NavLink>
+              </div>
+            </div>
           </Menu>
         </div>
       </div>
