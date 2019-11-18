2a948e5ee9a6cb89b0b13532e861482d73d0e366
9thtower <topfemo@gmail.com>
2019-11-18 09:36:48 +0100

    fixed mobile navbar header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Footer() {
-  return <div className="footer-container">footer</div>;
+  return (
+    <div className="footer-container">
+      footjjjeej d duddu dududududud efootjjjeej d duddu dududududud efootjjjeej
+      d duddu dududududud er
+    </div>
+  );
 }
