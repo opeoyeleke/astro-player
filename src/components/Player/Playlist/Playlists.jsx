664f374e577be465adba7c2f1429eddfda5e9e68
9thtower <topfemo@gmail.com>
2020-04-09 08:47:37 +0100

    removed react-soundplayer and made changes in colors of the player

--- src/components/Player/Playlist/Playlists.jsx
+++ src/components/Player/Playlist/Playlists.jsx
@@ -28,54 +28,58 @@ export default function Playlists() {
       the Almighty God who has given me the privilege to be alive, and I would
       like to express my sincere gratitude to my supervisor, Dr. A.O. Ojoawo for
       his patience and insightful correction of each manuscript that became this
-      work. Also, I'm extending my gratitude to Prof. MOB Olaogun, Prof. R.A.
-      Adedoyin, Dr. A.T. Onigbinde, Dr. M.O. Egwu, Dr. C.E. Mbada, Dr. T.O.
-      Awotidebe, Dr. (Mrs) А.О. Obembe and Mr. A. O. Olaoye who at one time or
-      the others contributed to this study and to my knowledge at large. Also,
-      my gratitude extends to all the non-academic staff of the Department of
-      Medical Rehabilitation, Obafemi Awolowo University, lle-Ife. My
-      appreciation also extends to the physiotherapists at Obafemi Awolowo
-      University Teaching Hospitals Complex (OAUTHC), lle-Ife for the knowledge
-      they passed on and discipline they instilled in me. Also, to Minerva 18'
-      class, I'm glad we made this journey together. My special thanks go to
-      Funmi, Seun, Doyin, Titus, Ayoleyi, Marvellous, Folarin and Joan for their
-      immense support in the course of this study. Most important, this study
-      would not meet its required end if not for the participants who consented
-      to taking part in this study. Lastly, my gratitude goes to my relatives;
-      Mayowa, Tolulope, Bunmi, Tolani, Damola, and Ayomide and my loving parents
-      for their financial support, moral support, affection and care. I
-      appreciate each and every one of you very much. Again, I say a big thank
-      you to God, who has given me the privilege to complete my study. Oyeleke
-      Opeyemi Foremost, my acknowledgement goes to the Almighty God who has
-      given me the privilege to be alive, and I would like to express my sincere
-      gratitude to my supervisor, Dr. A.O. Ojoawo for his patience and
-      insightful correction of each manuscript that became this work. Also, I'm
-      extending my gratitude to Prof. MOB Olaogun, Prof. R.A. Adedoyin, Dr. A.T.
-      Onigbinde, Dr. M.O. Egwu, Dr. C.E. Mbada, Dr. T.O. Awotidebe, Dr. (Mrs)
-      А.О. Obembe and Mr. A. O. Olaoye who at one time or the others contributed
-      to this study and to my knowledge at large. Also, my gratitude extends to
-      all the non-academic staff of the Department of Medical Rehabilitation,
-      Obafemi Awolowo University, lle-Ife. My appreciation also extends to the
-      physiotherapists at Obafemi Awolowo University Teaching Hospitals Complex
-      (OAUTHC), lle-Ife for the knowledge they passed on and discipline they
-      instilled in me. Also, to Minerva 18' class, I'm glad we made this journey
-      together. My special thanks go to Funmi, Seun, Doyin, Titus, Ayoleyi,
-      Marvellous, Folarin and Joan for their immense support in the course of
-      this study. Most important, this study would not meet its required end if
-      not for the participants who consented to taking part in this study.
-      Lastly, my gratitude goes to my relatives; Mayowa, Tolulope, Bunmi,
-      Tolani, Damola, and Ayomide and my loving parents for their financial
-      support, moral support, affection and care. I appreciate each and every
-      one of you very much. Again, I say a big thank you to God, who has given
-      me the privilege to complete my study. Oyeleke Opeyemi Foremost, my
-      acknowledgement goes to the Almighty God who has given me the privilege to
-      be alive, and I would like to express my sincere gratitude to my
-      supervisor, Dr. A.O. Ojoawo for his patience and insightful correction of
-      each manuscript that became this work. Also, I'm extending my gratitude to
-      Prof. MOB Olaogun, Prof. R.A. Adedoyin, Dr. A.T. Onigbinde, Dr. M.O. Egwu,
-      Dr. C.E. Mbada, Dr. T.O. Awotidebe, Dr. (Mrs) А.О. Obembe and Mr. A. O.
-      Olaoye who at one time or the others contributed to this study and to my
-      knowledge at large. Also, my gratitude extends to all the non-academic
+      work. Also, I'm ex
+      <div>
+        tending my gratitude to Prof. MOB Olaogun, Prof. R.A. Adedoyin, Dr. A.T.
+        Onigbinde, Dr. M.O. Egwu, Dr. C.E. Mbada, Dr. T.O. Awotidebe, Dr. (Mrs)
+        А.О. Obembe and Mr. A. O. Olaoye who at one time or the others
+        contributed to this study and to my knowledge at large. Also, my
+        gratitude extends to all the non-academic staff of the Department of
+        Medical Rehabilitation, Obafemi Awolowo University, lle-Ife. My
+        appreciation also extends to the physiotherapists at Obafemi Awolowo
+        University Teaching Hospitals Complex (OAUTHC), lle-Ife for the
+        knowledge they passed on and discipline they instilled in me. Also, to
+        Minerva 18' class, I'm glad we made this journey together. My special
+        thanks go to Funmi, Seun, Doyin, Titus, Ayoleyi, Marvellous, Folarin and
+        Joan for their immense support in the course of this study. Most
+        important, this study would not meet its required end if not for the
+        participants who consented to taking part in this study. Lastly, my
+        gratitude goes to my relatives; Mayowa, Tolulope, Bunmi, Tolani, Damola,
+        and Ayomide and my loving parents for their financial support, moral
+        support, affection and care. I appreciate each and every one of you very
+        much. Again, I say a big thank you to God, who has given me the
+        privilege to complete my study. Oyeleke Opeyemi Foremost, my
+        acknowledgement goes to the Almighty God who has given me the privilege
+        to be alive, and I would like to express my sincere gratitude to my
+        supervisor, Dr. A.O. Ojoawo for his patience and insightful correction
+        of each manuscript that became this work. Also, I'm extending my
+        gratitude to Prof. MOB Olaogun, Prof. R.A. Adedoyin, Dr. A.T. Onigbinde,
+        Dr. M.O. Egwu, Dr. C.E. Mbada, Dr. T.O. Awotidebe, Dr. (Mrs) А.О. Obembe
+        and Mr. A. O. Olaoye who at one time or the others contributed to this
+        study and to my knowledge at large. Also, my gratitude extends to all
+        the non-academic staff of the Department of Medical Rehabilitation,
+        Obafemi Awolowo University, lle-Ife. My appreciation also extends to the
+        physiotherapists at Obafemi Awolowo University Teaching Hospitals
+        Complex (OAUTHC), lle-Ife for the knowledge they passed on and
+        discipline they instilled in me. Also, to Minerva 18' class, I'm gl{" "}
+      </div>
+      ad we made this journey together. My special thanks go to Funmi, Seun,
+      Doyin, Titus, Ayoleyi, Marvellous, Folarin and Joan for their immense
+      support in the course of this study. Most important, this study would not
+      meet its required end if not for the participants who consented to taking
+      part in this study. Lastly, my gratitude goes to my relatives; Mayowa,
+      Tolulope, Bunmi, Tolani, Damola, and Ayomide and my loving parents for
+      their financial support, moral support, affection and care. I appreciate
+      each and every one of you very much. Again, I say a big thank you to God,
+      who has given me the privilege to complete my study. Oyeleke Opeyemi
+      Foremost, my acknowledgement goes to the Almighty God who has given me the
+      privilege to be alive, and I would like to express my sincere gratitude to
+      my supervisor, Dr. A.O. Ojoawo for his patience and insightful correction
+      of each manuscript that became this work. Also, I'm extending my gratitude
+      to Prof. MOB Olaogun, Prof. R.A. Adedoyin, Dr. A.T. Onigbinde, Dr. M.O.
+      Egwu, Dr. C.E. Mbada, Dr. T.O. Awotidebe, Dr. (Mrs) А.О. Obembe and Mr. A.
+      O. Olaoye who at one time or the others contributed to this study and to
+      my knowledge at large. Also, my gratitude extends to all the non-academic
       staff of the Department of Medical Rehabilitation, Obafemi Awolowo
       University, lle-Ife. My appreciation also extends to the physiotherapists
       at Obafemi Awolowo University Teaching Hospitals Complex (OAUTHC), lle-Ife
