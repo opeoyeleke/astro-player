34699b6384df1cf085bc2f7e186f24b95a386ae3
9thtower <topfemo@gmail.com>
2020-05-10 04:31:38 +0100

    disabled redux-logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger, thunk];
+const middlewares = [thunk];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
