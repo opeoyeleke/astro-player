5276e2cdfb44c58df513fdef215f57e92c7f3218
9thtower <topfemo@gmail.com>
2020-05-11 15:25:45 +0100

    few fixes

--- src/components/Player/ErrorBoundary/ErrorBoundary.jsx
+++ src/components/Player/ErrorBoundary/ErrorBoundary.jsx
@@ -4,7 +4,7 @@ import errorImage from "./../../../assets/FOeYt4E.png";
 export default function ErrorBoundary({ message }) {
   return (
     <div className="error-message">
-      <h1>Error! {message}</h1>
+      <h1>{message}</h1>
       <img src={errorImage} alt="error" />
       <h1>This Page is Buried in the Sand</h1>
     </div>
