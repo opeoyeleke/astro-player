f0543089783fb615dc6387a4773728457cb0c56f
9thtower <topfemo@gmail.com>
2021-02-23 23:08:25 +0100

    dotenv install and disable redux logger in prod

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,12 @@ import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger, thunk];
+let middlewares = [];
+if (process.env.NODE_ENV === "development") {
+  middlewares = [logger, thunk];
+} else {
+  middlewares = [thunk];
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
